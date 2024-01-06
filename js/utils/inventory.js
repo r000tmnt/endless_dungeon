68b10d261bf76236e2e8568fc7f45948da16a83f
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-01-06 20:23:09 +0800

    [Update] Import item data from database

--- js/utils/inventory.js
+++ js/utils/inventory.js
@@ -1,7 +1,8 @@
-/**
- * The complete data reference of all items in the bag
- */
-var inventory = []
+import weapon from '../dataBase/item/item_weapon.js'
+import armor from '../dataBase/item/item_armor.js'
+import key from '../dataBase/item/item_key.js'
+import potion from '../dataBase/item/item_potion.js'
+// and more ...
 
 /**
  * The item which you clicked
@@ -13,29 +14,30 @@ var selectedItem = {}
  * @param {number} itemType - A number represent the type of the item 
  * @returns A string of routes to get item data
  */
-const getItemUrl = (itemType) => {
-    let requestUrl = ''
-    switch(itemType){
+const getItemType = (item) => {
+    let data = {}
+    switch(item.type){
         case 0:
-            requestUrl = '../../assets/data/item/item_potion.json'
+            data = potion.getOne(item.id)
         break;
         case 1:
         break;
         case 2:
         break;
         case 3:
-            requestUrl = '../../assets/data/item/item_weapon.json'
+            data = weapon.getOne(item.id)
         break;
         case 4:
-            requestUrl = '../../assets/data/item/item_armor.json'
+            data = armor.getOne(item.id)
         break;
         case 5:
         break;
         case 6:
+            data = key.getOne(item.id)
         break;
     }
 
-    return requestUrl
+    return data
 }
 
 const showItemToolTip = (currentActingPlayer, hoverItem) => {
@@ -61,9 +63,7 @@ const showItemToolTip = (currentActingPlayer, hoverItem) => {
         } 
     }
     
-
-
-    // toolTips[selectedItem.index].style.visibility = 'visible'
+    toolTips[selectedItem.index].style.visibility = 'visible'
 }
 
 /**
@@ -71,7 +71,7 @@ const showItemToolTip = (currentActingPlayer, hoverItem) => {
  * @param {object} currentActingPlayer - An object represent current acting player 
  * @param {object} clickedItem - An object represent the selected item  
  */
-const openItemSubManu = (currentActingPlayer, clickedItem) =>{
+const openItemSubMenu = (currentActingPlayer, clickedItem) =>{
     const subMenu = document.getElementById('itemAction')
     const desc = document.getElementById('item-desc')
     const itemActions = subMenu.querySelectorAll('li')
@@ -86,14 +86,14 @@ const openItemSubManu = (currentActingPlayer, clickedItem) =>{
         // If the item is a weapon or armor
         const { equip } = currentActingPlayer
 
-        const equipted = Object.entries(equip).findIndex(e => e.id === clickedItem.id)
+        const equipped = Object.entries(equip).findIndex(e => e.id === clickedItem.id)
 
         itemActions.forEach(i => {
             if(i.dataset.action === 'use'){
                 i.style.display = 'none'
             }
 
-            if(equipted >= 0 && i.dataset.action === 'equip'){
+            if(equipped >= 0 && i.dataset.action === 'equip'){
                 i.innerText = 'Unequip'
                 i.addEventListener('click', () => {
                     UnequipItem(currentActingPlayer)
@@ -192,10 +192,11 @@ const giveItem = (currentActingPlayer) => {
     // TODO: Give item to another player
 }
 
-export const clearInventory = () => {
-    // Clear out inventory reference
-    inventory.splice(0)
+export const resizeInventory = () => {
 
+}
+
+export const clearInventory = () => {
     const subMenu = document.getElementById('itemAction')
 
     // Close sub menu
@@ -209,7 +210,6 @@ export const clearInventory = () => {
     while(space.firstChild){
         space.removeChild(space.firstChild)
     }
-    
 }
 
 /**
@@ -220,6 +220,8 @@ export const clearInventory = () => {
 export const constructInventoryWindow = async(currentActingPlayer, canvasPosition) => {
     // Get UI elements
     const Inventory = document.getElementById('item')
+    const title = Inventory.children[0]
+    const filterButton = document.querySelectorAll('.filter')
     const space = document.getElementById('inventory')
     const subMenu = document.getElementById('itemAction')
     const itemActions = subMenu.querySelectorAll('li')
@@ -229,103 +231,83 @@ export const constructInventoryWindow = async(currentActingPlayer, canvasPositio
         const item = document.createElement('div')
         const itemCount = document.createElement('div')
         const itemToolTip = document.createElement('span')
-        // Get item url
-        let requestUrl = getItemUrl(currentActingPlayer.bag[i].type)
-
         // Get item data
-        const response = await fetch(requestUrl)
+        const itemData = getItemType(currentActingPlayer.bag[i])
 
-        try {
-            const result = await response.json()
-            console.log(result)
-            console.log(typeof result)
-    
-            const items = Object.values(result)
-    
-            console.log(items)
-    
-            // Set inner text with item data
-            for(let j=0; j < items.length; j++){
-                if(items[j].id === currentActingPlayer.bag[i].id){
-                    inventory.push(items[j])
+        item.setAttribute('data-id', itemData.id)
+        item.setAttribute('data-type', itemData.type)
+        item.setAttribute('data-limit', itemData.stackLimit)
+        item.setAttribute('data-index', i)
 
-                    item.setAttribute('data-id', items[j].id)
-                    item.setAttribute('data-type', items[j].type)
-                    item.setAttribute('data-limit', items[j].stackLimit)
-                    item.setAttribute('data-index', i)
+        itemToolTip.classList.add('item-toolTip')
 
-                    itemToolTip.classList.add('item-toolTip')
+        // Set long-press event
+        item.addEventListener('long-press', () => {
+            selectedItem = itemData
+            selectedItem['index'] = i
+            openItemSubMenu( currentActingPlayer, itemData)
+        })
 
-                    // Set long-press event
-                    item.addEventListener('long-press', () => {
-                        selectedItem = currentActingPlayer.bag[i]
-                        selectedItem['index'] = i
-                        openItemSubManu( currentActingPlayer, items[j])
-                    })
+        // Set hover event
+        item.addEventListener('hover', () => {
+            showItemToolTip(currentActingPlayer, itemData)
+        })
 
-                    // Set hover event
-                    item.addEventListener('hover', () => {
-                        selectedItem = currentActingPlayer.bag[i]
-                        selectedItem['index'] = i
-                        showItemToolTip(currentActingPlayer, items[j])
-                    })
+        // Setting inner text
+        item.innerText = itemData.name
+        itemCount.innerText = currentActingPlayer.bag[i].amount
 
-                    // Setting inner text
-                    item.innerText = items[j].name
-                    itemCount.innerText = currentActingPlayer.bag[i].amount
+        // Check if item equipped
+        if(itemData.type === 3 || itemData.type === 4){
+            const equipped = Object.entries(currentActingPlayer.equip).findIndex(e => e.id === itemData.id)
 
-                    // Check if item equipped
-                    if(items[j].type === 3 || items[j].type === 4){
-                        const equipted = Object.entries(currentActingPlayer.equip).findIndex(e => e.id === items[j].id)
-
-                        if(equipted >= 0){
-                            // Prepare to show a little text on the bottom left of the block
-                            const equipBadge = document.createElement('div')
-                            equipBadge.innerText = 'E'
-                            equipBadge.style.width = 'fit-content'
-                            item.append(itemCount)
-                        }
-                    }
-
-                    break
-                }
+            if(equipped >= 0){
+                // Prepare to show a little text on the bottom left of the block
+                const equipBadge = document.createElement('div')
+                equipBadge.classList.add('.item-equip')
+                equipBadge.innerText = 'E'
+                equipBadge.style.width = 'fit-content'
+                item.append(equipBadge)
             }
-    
-            console.log(item)
-            
-            // Set the size of each block
-            const itemBlockSize = Math.floor(canvasPosition.width / 100) * 33
-            const itemBlockMargin = Math.floor((itemBlockSize / 100) * 10)
-
-            // Apply size number
-            // itemCount.style.position = 'absolute'
-            itemCount.style.width = 'fit-content'
-            itemCount.classList.add('item-count')
-
-            item.style.width = itemBlockSize + 'px'
-            item.style.height = itemBlockSize + 'px'
-
-            // If the index is the middle column
-            if(((i + (i+1)) % 3) === 0){
-                item.style.margin = `0 ${itemBlockMargin}px`
-            }
-            
-            item.classList.add('item')
-
-            space.style.padding = itemBlockMargin + 'px'
-
-            // Appen child to element
-            item.append(itemToolTip)
-            item.append(itemCount)
-            space.append(item)
-
-            // Display inventory
-            Inventory.classList.remove('invisible')
-            Inventory.classList.add('open_window')
-        } catch (error) {
-            console.log(error)
-            return error
         }
+
+        console.log(item)
+            
+        // Set the size of each block
+        const fontSize = Math.floor( 10 * Math.floor(canvasPosition.width / 100))
+        const itemBlockSize = Math.floor(canvasPosition.width / 100) * 26
+        const itemBlockMargin = Math.floor((itemBlockSize / 100) * 10)
+
+        // Apply size number
+        title.style.fontSize = fontSize + 'px'
+        title.style.paddingBottom = (fontSize / 2) + 'px'
+        filterButton.forEach(f => {
+            f.style.fontSize = Math.floor(fontSize / 3) + 'px'
+            f.style.width = "22%"
+        })
+
+        itemCount.style.width = 'fit-content'
+        itemCount.style.fontSize = (fontSize / 2) + 'px'
+        itemCount.classList.add('item-count')
+
+        item.style.width = itemBlockSize + 'px'
+        item.style.height = itemBlockSize + 'px'
+        item.style.fontSize = (fontSize / 2) + 'px'
+        item.style.border = '1px dotted white'
+
+        // If the index is the middle column
+        if(((i + (i+1)) % 3) === 0){
+            item.style.margin = `0 ${itemBlockMargin}px`
+        }
+        
+        item.classList.add('item')
+
+        space.style.padding = itemBlockMargin + 'px'
+
+        // Append child to element
+        item.append(itemToolTip)
+        item.append(itemCount)
+        space.append(item)
     }
 
     // Set click event to sub menu buttons
@@ -359,4 +341,8 @@ export const constructInventoryWindow = async(currentActingPlayer, canvasPositio
             break;
         }
     }
+
+    // Display inventory
+    Inventory.classList.remove('invisible')
+    Inventory.classList.add('open_window')
 }
