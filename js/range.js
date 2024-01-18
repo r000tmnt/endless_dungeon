6b21e6e111b41fda46ad89878037c9fcff6fa0cd
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-01-18 11:53:49 +0800

    [Update] Use the same for loop to draw selectable tiles

--- js/range.js
+++ js/range.js
@@ -2,28 +2,36 @@ export default class Range {
     constructor(map, tileSize){
         this.map = map
         this.tileSize = tileSize
+        this.fillStyle = ''
     }
 
-    draw(ctx, selectableSpace, actionMode){
+    draw(ctx, selectableSpace, actionMode, skillType){
 
         // Change the globalCompositeOperation to destination-over so that anything
         // that is drawn on to the canvas from this point on is drawn at the back
         // of what's already on the canvas
-        switch(actionMode){
-            case 'move':
-                if(selectableSpace.length){
-                    ctx.save()
-                    ctx.globalCompositeOperation = 'destination-over';
-                    this.#showMovableSpace(ctx, selectableSpace)
-                }
-            break;
-            case 'attack': case 'skill':
-                if(selectableSpace.length){
-                    ctx.save()
-                    ctx.globalCompositeOperation = 'destination-over';
-                    this.#showAttackRange(ctx, selectableSpace)
-                }
-            break;
+        if(selectableSpace.length){
+            ctx.save()
+            ctx.globalCompositeOperation = 'destination-over';     
+            
+            switch(actionMode){
+                case 'move':
+                    this.fillStyle = 'green'
+                break;
+                case 'attack': case 'skill':
+                    this.fillStyle = 'rgba(255, 0, 0, 0.5)'
+                    
+                    if(skillType === 'defence'){
+                        this.fillStyle = 'rgba(0, 0, 255, 0.5)'
+                    }
+                break;
+                case 'item':
+                    this.fillStyle = 'rgba(0, 0, 255, 0.5)'
+
+                break;
+            }   
+            
+            this.#showSelectableSpace(ctx, selectableSpace)
         }
     }
 
@@ -32,12 +40,12 @@ export default class Range {
     }
 
     // show a range of walkable space
-    #showMovableSpace(ctx, walkableSpace){
-        for(let layer = 0; layer < walkableSpace.length; layer++){
-            for(let block = 0; block < walkableSpace[layer].length; block++){
-                const row = walkableSpace[layer][block][0]
-                const col = walkableSpace[layer][block][1]
-                ctx.fillStyle = 'green';
+    #showSelectableSpace(ctx, selectableSpace){
+        for(let layer = 0; layer < selectableSpace.length; layer++){
+            for(let block = 0; block < selectableSpace[layer].length; block++){
+                const row = selectableSpace[layer][block][0]
+                const col = selectableSpace[layer][block][1]
+                ctx.fillStyle = this.fillStyle;
                 ctx.fillRect( // x, y, width, height
                     col * this.tileSize,
                     row * this.tileSize,
@@ -46,24 +54,6 @@ export default class Range {
                 );               
             }
         }
-        ctx.restore()
-    }
-
-    // show the range of the attack
-    #showAttackRange(ctx, attackRange){
-        for(let layer = 0; layer < attackRange.length; layer++){
-            for(let block = 0; block < attackRange[layer].length; block++){
-                const row = attackRange[layer][block][0]
-                const col = attackRange[layer][block][1]
-                ctx.fillStyle = 'rgba(255, 0, 0, 0.5)';
-                ctx.fillRect( // x, y, width, height
-                    col * this.tileSize,
-                    row * this.tileSize,
-                    this.tileSize,
-                    this.tileSize
-                );                
-            }
-        } 
         ctx.restore()
     }
 }
