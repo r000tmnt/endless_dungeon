b26eacf5a71cfdd5cfbbdaa3cddef6d7ff2b434e
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-01-19 13:27:32 +0800

    [FIX] Player position x axis

--- js/utils/inventory.js
+++ js/utils/inventory.js
@@ -126,7 +126,7 @@ const setItemSpace = async(currentActingPlayer, enemyPosition, tileMap) => {
     const Inventory = document.getElementById('item')
     const itemUseRange = await getAvailableSpace(
         tileMap,
-        { row: parseInt(currentActingPlayer.y / currentActingPlayer.tileSize), col: parseInt(currentActingPlayer.y / currentActingPlayer.tileSize) },
+        { row: parseInt(currentActingPlayer.y / currentActingPlayer.tileSize), col: parseInt(currentActingPlayer.x / currentActingPlayer.tileSize) },
         selectedItem.effect.range, selectedItem.effect?.dmg? null: enemyPosition
     )
 
