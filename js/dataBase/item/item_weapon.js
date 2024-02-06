b43d75f0725192e409c5e420ad0affd06a87d39f
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-02-06 14:36:33 +0800

    [Update] Create a knife

--- js/dataBase/item/item_weapon.js
+++ js/dataBase/item/item_weapon.js
@@ -18,6 +18,25 @@ export default {
             },
             "prefix": [],
             "suffix": []
+        },
+        {
+            "id": "knife_1",
+            "type": 3,
+            "stackLimit": 1,
+            "name": "Combat knife",
+            "position": "hand",
+            "effect": {
+                "base_damage": {
+                    "min": 1,
+                    "max": 3
+                },
+                "base_attribute": {
+                    "str": 2
+                },
+                "desc": "A small knife for self defense"
+            },
+            "prefix": [],
+            "suffix": []
         }
     ],
 
