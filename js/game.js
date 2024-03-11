e28940c6195385db80214c8becf3e9f2d63bb589
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-03-11 14:13:37 +0800

    [Fix] Pass the camera size and font size to setConversationEvent

--- js/game.js
+++ js/game.js
@@ -84,16 +84,16 @@ class Game{
         }else{
             switch(this.level.phase[this.phaseCount]){
                 case 'conversation':
+                    const { cameraWidth, cameraHeight } = redefineDeviceWidth()
+                    const { fontSize, fontSize_md, fontSize_sm } = redefineFontSize(cameraWidth)
+
                     if(this.textBox !== null){
                         this.textBox.event = this.level.event[0].scene
                     }else{
                         // Defined textBox object and event for the first load
                         this.textBox = new TextBox(this.level.event[0].scene)
-                        this.textBox.setConversationEvent()
+                        this.textBox.setConversationEvent(cameraWidth, cameraHeight, fontSize_md)
                     }
-                    
-                    const { cameraWidth, cameraHeight } = redefineDeviceWidth()
-                    const { fontSize, fontSize_md, fontSize_sm } = redefineFontSize(cameraWidth)
         
                     this.textBox.setConversationWindow(cameraWidth, cameraHeight, fontSize, fontSize_md, fontSize_sm)
                 break;
@@ -157,7 +157,8 @@ class Game{
                 case 'intermission':
                 break;
                 case 'end':
-
+                    toggleCanvas(false)
+                    displayTitleScreen()
                 break;
             }
         }
