adfd50443341c10ea04cbb2f601a21a9e5efcaef
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-03-25 18:07:03 +0800

    [WIP] Preparing event triggered on languague changed

--- js/utils/i18n.js
+++ js/utils/i18n.js
@@ -1,6 +1,7 @@
 import i18next from "i18next";
 import * as en from '../locale/en.json'
 import * as zh from '../locale/zh.json'
+import game from "../game";
 
 i18next.init({
     lng: 'en',
@@ -13,6 +14,12 @@ i18next.init({
             translation: zh
         }
     }
+})
+
+// on language change event
+i18next.on('languageChanged', (lng) => {
+    // Re-render ui elements
+    
 })
 
 export const t = i18next.t
