c1ffb34894f531a2178bd31681176e0b811ce242
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-03-26 16:30:55 +0800

    [Update] Import item localization into resource

--- js/locale/en.js
+++ js/locale/en.js
@@ -1,9 +1,23 @@
 import tutorial_1 from './level/p-1-1-en'
 import ui from './ui/ui_en'
+import potion from './item/item_potion_en'
+import weapon from './item/item_weapon_en'
+import armor from './item/item_armor_en'
+import material from './item/item_material_en'
+import key from './item/item_key_en'
+import other from './item/item_other_en'
 
 export default {
     tutorial_1,
     ui,
+    "item": {
+        ...potion,
+        ...weapon,
+        ...armor,
+        ...material,
+        ...key,
+        ...other
+    },
     "demo": "This is the end of the demo.\nThank you for playing the game.",
     "loading": "Loading...",
     "tap": "TAP TO CONTINUE",
--- js/locale/zh.js
+++ js/locale/zh.js
@@ -1,9 +1,23 @@
 import tutorial_1 from './level/p-1-1-zh'
 import ui from './ui/ui_zh'
+import potion from './item/item_potion_zh'
+import weapon from './item/item_weapon_zh'
+import armor from './item/item_armor_zh'
+import material from './item/item_material_zh'
+import key from './item/item_key_zh'
+import other from './item/item_other_zh'
 
 export default {
     tutorial_1,
     ui,
+    "item": {
+        ...potion,
+        ...weapon,
+        ...armor,
+        ...material,
+        ...key,
+        ...other
+    },
     "demo": "這裡是 DEMO 的終點。 感謝你的遊玩",
     "loading": "讀取中...",
     "tap": "點擊螢幕繼續",
