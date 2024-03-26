7d5b873aaddb5ee95da841d01a90dba528d12116
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-03-26 13:30:00 +0800

    [Update] Impoer tutorial and ui resource from js instead of json

---
+++ js/locale/en.js
@@ -0,0 +1,11 @@
+import tutorial_1 from './level/p-1-1-en'
+import ui from './ui/ui_en'
+
+export default {
+    tutorial_1,
+    ui,
+    "demo": "This is the end of the demo.\nThank you for playing the game.",
+    "loading": "Loading...",
+    "tap": "TAP TO CONTINUE",
+    "back": "Back"
+}
---
+++ js/locale/zh.js
@@ -0,0 +1,11 @@
+import tutorial_1 from './level/p-1-1-zh'
+import ui from './ui/ui_zh'
+
+export default {
+    tutorial_1,
+    ui,
+    "demo": "這裡是 DEMO 的終點。 感謝你的遊玩",
+    "loading": "讀取中...",
+    "tap": "點擊螢幕繼續",
+    "back": "返回"
+}
