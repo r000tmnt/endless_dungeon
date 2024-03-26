a3e5c305b2eda43fc60f7ae1fd69a07a82001963
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-03-26 13:28:43 +0800

    [Update] ui text localization resource

---
+++ js/locale/ui/ui_en.js
@@ -0,0 +1,71 @@
+export default {
+    "action": {
+        "move": "Move",
+        "attack": "Attack",
+        "skill": "Skill",
+        "item": "Item",
+        "pick": "Pick",
+        "status": "Status",
+        "stay": "Stay"
+    },
+    "config": {
+        "bgm": "BGM",
+        "se": "SE",
+        "grid": "Grid",
+        "on": "Display",
+        "off": "Hide",
+        "filter": "Filter",
+        "default": "Default",
+        "retro": "Retro",
+        "language": "Language"
+    },
+    "conversation": {
+        "skip": "SKIP",
+        "hide": "HIDE",
+        "auto": "AUTO",
+        "log": "LOG"
+    },
+    "inventory": {
+        "title": "Inventory",
+        "filter": {
+            "quiptment": "Equipment",
+            "material": "material",
+            "key": "Key"
+        },
+        "subMenu": {
+            "use": "Use",
+            "equip": "Equip",
+            "drop": "Drop",
+            "give": "Give",
+            "close": "Close"
+        },
+        "range": {
+            "cancel": "Cancel",
+            "confirm": "Confirm"
+        },
+        "pick": {
+            "title": "Pick up",
+            "all": "Take All",
+            "confirm": "confirm"
+        }
+    },
+    "objective":{
+        "victory": "Victory",
+        "fail": "Fail",
+        "optional": "Optional"
+    },
+    "option": {
+        "party": "Party",
+        "objective": "Objective",
+        "config": "Config",
+        "end": "End Turn"
+    },
+    "result": {
+        "win": "WIN",
+        "lose": "LOSE",
+        "stash": "Push items to the stash",
+        "pickAfterBattle": "Pick up items",
+        "finish": "Finish",
+        "warn": "Items left on the field will be unavailable once you proceed to the next phase."
+    }
+}
