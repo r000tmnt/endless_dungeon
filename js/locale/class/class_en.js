dad0db196ab2321c23cf37ab981b8bf402fc3eaa
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-03-27 13:48:39 +0800

    [Create] Prepare class name localization resource

---
+++ js/locale/class/class_en.js
@@ -0,0 +1,9 @@
+export default {
+    "class_fighter_1": "Fighter",
+
+    "mob_zombie_1": "Zombie",
+    "undead": "Undead",
+    "gravedigger": "Grave Digger",
+    "spirit": "Spirit",
+    "cavewolf": "Cave Wolf"
+}
---
+++ js/locale/class/class_zh.js
@@ -0,0 +1,9 @@
+export default {
+    "class_fighter_1": "戰士",
+
+    "mob_zombie_1": "殭屍",
+    "undead": "不死者",
+    "gravedigger": "盜墓者",
+    "spirit": "靈體",
+    "cavewolf": "窟狼"
+}
