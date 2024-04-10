f198735c33590761a7066db2c2096e856a9f6038
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-04-10 10:57:31 +0800

    [WIP] Working on the dialogues

---
+++ js/locale/level/tutorial_2_en.js
@@ -0,0 +1,49 @@
+export default {
+    "title": "Lack of keys",
+    "dialogue_1": "A corridor.\nA different place.\nIt seems so out of place to where you've beem moments ago.",
+    "dialogue_2": "The key shatter to dust once you across the door.\nThankfully,there are torches hanging on the wall.",
+    "dialogue_3": "You heard a knocking sound from the distance not far way and a shadow moving.\nWith cautious, you process the road ahead.",
+    "dialogue_4": "Hey you! You're not the monster right?\nHave you find a small key? Mine got stollen by those creature.\nThe voice came from the door near where you standing.",
+    // Got the key or not
+    "option_1": {
+        "value": "Open the door",
+        "content": "You see a man with a bow on his hand. He seems relief from the fact that he is free from the dusty room he's beem stuck with."
+    },
+    "option_2": {
+        "value": "Walk away quietly",
+        "content": "Wait. Come back! You...\nYou don't seems to mind the fact that voice sound desperate and keep finding your way out."
+    },
+    // A選項後續
+    "dialogue_5-1": "Thanks. I thought you'll run away and I'm going to stay like this forever.\nThe name is... by the way.",
+    "dialogue_6-1": "Do you got more keys in your bag would you?\n{archer_name} saying it like it is urgent.",
+    // B選項後續
+    "dialogue_5-2": "Something is wracked. A man bustout the door you passed not long ago. You can tell that his angry at you.",
+    "dialogue_6-2": "You see a man holding a bow, taking position to fire an arrow to you.",
+    "option_3":{
+        "value": "Oh Fuc...!",
+        "content": "You pull out the knife just in time to block the arrow.\nstill, the shrap sourface scrach your skin."
+    },
+    "option_4":{
+        "value": "Keep running",
+        "content": "You pick up the pace. Running like your life depends on it."
+    },
+    "dialogue_7": "You bastard. You left me to die!\nThe man yall at you. Not listen anything you saied that explains your action. It seem a fight is inevitable.",
+    // 戰鬥事件結束
+    "dialogue_8": "Alright alright. You win, just take the money. Don't kill me.",
+    "dialogue_9": "The man sitting on ground. Waiting for you to take the prize.",
+    "dialogue_10": "What are you looking at? Didn't you lock me in there to break my mind so you can take my money?",
+    // 戰鬥結束
+    "dialogue_11": "You explain to him what happend in the cave and you stunble your way here by the key you got by kill the monster. That's the only key you got.",
+    // A選項後續
+    "dialogue_12": "That sounds rough. You don't want to go mindless alone, I would tell you that.",
+    // B選項後續
+    "dialogue_12-1": "That sounds rough. I thought you were try to kill me. Sorry about that",
+    "dialogue_13": "The villige is not far from here. I can take you there if you want.",
+    "dialogue_14": "The man offering you a guide. From the situation you have, it seems to be reasonable choice to take the offer.",
+    "dialogue_15": "Let's go hunting first. We need at leat two keys to get out here.",
+    // "dialogue_16": "Suit yourself, I'm out of here",
+    // "dialogue_17": "As you defeat the monster.\nA small object made out of metal drop on the ground. It glooms, and lights up the space around it.",
+    // "dialogue_18": "It appears to be in a shape of key. At least that's what you can think of.",
+    // "dialogue_19": "You take a closer look at the key.\nNow it is in your possession, you feel the surge of energy lays inside it.",
+    // "dialogue_20": "With the lights from the key.\nYou're able to find a door in the end of the tunnel.\nAlthough you're not sure about the use of this key in your hand. You insert it into the key hole and open the door."
+}
