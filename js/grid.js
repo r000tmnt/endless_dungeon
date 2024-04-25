80785972658dd37f24c3fa6b88505295d4166d6e
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-04-25 09:12:32 +0800

    [Update] Reduce the time to check if pointedBlock is set

--- js/grid.js
+++ js/grid.js
@@ -7,27 +7,27 @@ export default class Grid {
 
     draw(ctx){
         for(let layer = 0; layer < this.map.length; layer++){
+            ctx.strokeStyle = "rgba(211, 211, 211, .7)";
             for(let block = 0; block < this.map[layer].length; block++){
-                ctx.strokeStyle = "rgba(211, 211, 211, .7)";
                 ctx.strokeRect(
                     block * this.tileSize,
                     layer * this.tileSize,
                     this.tileSize,
                     this.tileSize
                 );
-
-                if(this.pointedBlock !== null && Object.entries(this.pointedBlock).length){
-                    // Draw border around the pointed block
-                    const { row, col } = this.pointedBlock
-                    ctx.strokeStyle = "yellow";
-                    ctx.strokeRect(
-                    col * this.tileSize,
-                    row * this.tileSize,
-                    this.tileSize,
-                    this.tileSize
-                    );
-                }
             }
+        }
+
+        if(this.pointedBlock !== null && Object.entries(this.pointedBlock).length){
+            // Draw border around the pointed block
+            const { row, col } = this.pointedBlock
+            ctx.strokeStyle = "yellow";
+            ctx.strokeRect(
+                col * this.tileSize,
+                row * this.tileSize,
+                this.tileSize,
+                this.tileSize
+            );
         }
     }
 
