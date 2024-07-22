b18625c5a8ffeed1062a3ec00c6a6d72cee98612
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-07-22 15:51:04 +0800

    [Updare] Complete with setting up skill list and resize function

--- js/components/skillMenu.js
+++ js/components/skillMenu.js
@@ -1,3 +1,10 @@
+import { t } from "../utils/i18n"
+import setting from "../utils/setting"
+import game from "../game"
+import skills from "../dataBase/skill"
+import skill_type from "../dataBase/skill/skill_type"
+import { displayUIElement, resizeHiddenElement } from '../utils/ui'
+
 export default class SkillMenu extends HTMLElement {
     static observedAttributes = ["show"]
 
@@ -14,10 +21,52 @@ export default class SkillMenu extends HTMLElement {
 
     attributeChangedCallback(name, oldValue, newValue) {
         if(newValue === 'true'){
-            
-            this.list.innerHTML = `
-                <li></li>
-            `
+            const { fontSize_md, fontSize_sm, tileSize, camera } = setting.general
+            const { width, height } = camera
+
+            resizeHiddenElement(this.style, width, height, fontSize_sm)
+
+            this.list.innerHTML = game.inspectingCharacter.skill.map(skill => {
+                const skillData = skills.getOne(skill)
+
+                return `
+                    <li class="skill flex bg-black"
+                        style="font-size:${fontSize_md}px; box-sizing:border-box; padding:${fontSize_sm}px;${this.ifSkillAvailable(skillData)? 'color:grey' : '' }"
+                        data-skill="${JSON.stringify(skillData)}">
+                        <img src="" alt="icon"
+                            class="icon"
+                            style="width:${tileSize}px; height:${tileSize}px; margin-right:${fontSize_sm}px"/>
+                        <div class="w-full">
+                            <div class="flex" style="justify-content:space-between">
+                                <sapn>
+                                    ${ t(`skill.${skillData.id}.name`) }
+                                </sapn>
+                                <span>
+                                    ${skillData.cost.attribute}: ${skillData.cost.value}
+                                </span>
+                            </div>
+                            <div>
+                                ${t(`skill.${skillData.id}.desc`)}
+                            </div>
+                        </div>
+                    </li>
+                `
+            })
+
+            // Bind click event
+            document.querySelectorAll(".skill").forEach(s => {
+                if(s.style.color !== 'grey'){
+                    s.addEventListener('click', () => {
+                        game.action.useSkillasync(JSON.parse(s.dataset.skill)).then(() => {
+                            this.setAttribute("show", false)
+                        })
+                    })
+                }else{
+                    s.addEventListener('click', () => {
+                        console.log("Not enough resouce")
+                    })
+                }
+            })
 
             this.classList.remove('invisible')
             this.classList.add('open_window')
@@ -28,16 +77,62 @@ export default class SkillMenu extends HTMLElement {
     }
 
     render(){
+        const { fontSize, fontSize_md, fontSize_sm } = setting.general
+        const { itemBlockSize } = setting.inventory
+
         this.innerHTML = `
-            <div>Skill</div>
+            <div style="font-size:${fontSize}px">
+                ${ t("ui.action.skill") }
+            </div>
 
-            <button class="back absolute" data-action="skill" type="button">BACK</button>
+            <button 
+                class="back absolute" 
+                data-action="skill" 
+                type="button"
+                style="transform: translateX(-${fontSize_sm}px);top: ${fontSize_sm}px;font-size: ${fontSize_md}px" >
+                    ${t('back')}
+                </button>
 
-            <ul class="learned-skills disable-scrollbars">
+            <ul class="learned-skills disable-scrollbars"
+                style="max-height:${itemBlockSize * 3}px">
                 <!-- skills -->
             </ul>
         `
 
         this.list = this.querySelector(".learned-skills")
+
+        // Close button event
+        this.children[1].addEventListener("click", () => {
+            game.action.mode = ''
+            this.setAttribute("show", false)
+            displayUIElement()
+        })
+    }
+
+    ifSkillAvailable(skillData){
+        if(game.inspectingCharacter.totalAttribute[skillData.cost.attribute] >= skillData.cost.value && game.inspectingCharacter.equip.hand?.id.includes(skill_type[skillData.type]) ||
+        skill_type[skillData.type] === 'none' || skill_type[skillData.type] === 'status'){
+            return true
+        }else{
+            return false
+        }
+    }
+
+    resize(){
+        const { fontSize, fontSize_md, fontSize_sm, tileSize } = setting.general
+        const { itemBlockSize } = setting.inventory
+
+        this.children[0].style.fontSize = fontSize + 'px'
+        this.list.style.maxHeight = (itemBlockSize * 3) + 'px'
+
+        document.querySelectorAll(".skill").forEach(skill => {
+            skill.style.fontSize = fontSize_md + 'px'
+            skill.style.padding = `${fontSize_sm}px` 
+            skill.children[0].style.width = tileSize + 'px'
+            skill.children[0].style.height = tileSize + 'px'
+            skill.children[0].style.marginRight = `${fontSize_sm}px`
+        })
     }
 }
+
+customElements.define("skill-menu", SkillMenu)
