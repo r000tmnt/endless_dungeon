5b8ff4049cf00742b7849fab8557c72bc8fd1c91
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2024-07-22 15:53:26 +0800

    [Update] Do not import skill types

--- js/dataBase/skill.js
+++ js/dataBase/skill.js
@@ -1,28 +1,25 @@
 import skill_knife from "./skill/skill_knife"
 import skill_status from "./skill/skill_status"
 import skill_sword from "./skill/skill_sword"
-import skill_type from "./skill/skill_type"
 
 
-            export default {
-                data: [
-                    ...skill_knife,
-...skill_status,
-...skill_sword,
-...skill_type,
+export default {
+    data: [
+        ...skill_knife,
+        ...skill_status,
+        ...skill_sword,
+    ],
 
-                ],
+    getAll(){
+        return this.data
+    },
 
-                getAll(){
-                    return this.data
-                },
+    getOne(id){
+        return this.data.find(s => s.id === id)
+    },
 
-                getOne(id){
-                    return this.data.find(s => s.id === id)
-                },
-
-                search(keyWord){
-                    return this.data.filter(s => s.name.includes(keyWord))
-                }
-            }
+    search(keyWord){
+        return this.data.filter(s => s.name.includes(keyWord))
+    }
+}
             
